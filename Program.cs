521ec4da08aae8ebe961ac70d186b235aeb93ad0
Andrea <chela355@hotmail.com>
2021-03-01 18:27:19 -0500

    se hizo la tarea

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Tiempo_de_viaje
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Duracion Tramo: ");
+            int Tramo = Convert.ToInt32(Console.ReadLine());
+            int hora = 60;
+            int tiempoTotalViaje= 0;
+            while (Tramo != 0)
+            {
+                tiempoTotalViaje += Tramo;
+                Console.WriteLine("Duracion Tramo: ");
+                Tramo = Convert.ToInt32(Console.ReadLine());
+            }
+            
+            int horasdeViaje= tiempoTotalViaje / hora;
+            int minutosdeViaje= tiempoTotalViaje % hora;
+            Console.WriteLine("Tiempo total de viaje: " + horasdeViaje + " :" + minutosdeViaje);
+
+            
+            
+        }
+
+    }
+}
